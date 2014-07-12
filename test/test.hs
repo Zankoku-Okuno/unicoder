26ce13981fb490935c7277bbd25e46e296627e33
Zankoku Okuno <zankoku.okuno@gmail.com>
2014-07-12 14:16:01 -0400

    added a test suite

---
+++ test/test.hs
@@ -0,0 +1,27 @@
+import System.IO
+import System.Exit
+import Control.Monad
+import qualified Data.Text.IO as T
+
+import Text.Unicoder
+
+main :: IO ()
+main = do
+	m_config <- parseConfigFile "test/test.config"
+	testFile <- case m_config of
+		Nothing -> die "Could not parse config file."
+		Just config -> return $ testFile config
+	let files = ["passthrough", "mono", "di"]
+	results <- mapM testFile files
+	unless (all id results) $ do
+		mapM_ putStrLn $ zipWith (\a b -> a ++ ": " ++ if b then "OK" else "FAILURE") files results
+		exitFailure
+
+testFile :: Config -> FilePath -> IO Bool
+testFile config path = do
+	input <- T.readFile $ "test/" ++ path ++ ".in"
+	output <- T.readFile $ "test/" ++ path ++ ".out"
+	return $ unicodize config input == output
+
+die :: String -> IO a
+die msg = putStrLn msg >> exitFailure
